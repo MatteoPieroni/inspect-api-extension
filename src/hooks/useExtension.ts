01a20bb16078ea211fc4e10f417bd1e3eeb653b1
Matteo Pieroni <matteopieroni6@gmail.com>
2020-03-31 22:38:37 +0100

    Connected to background

---
+++ src/hooks/useExtension.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import { DataCollection } from "../types";
+
+export const useExtension = () => {
+  const [data, setData] = useState<DataCollection | null>(null);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    chrome.runtime.connect();
+
+    const getBackgroundData: (data: DataCollection) => void = data => {
+      try {
+        setData(data);
+      } catch (e) {
+        setError(e);
+      }
+    };
+
+    chrome.runtime.onMessage.addListener(getBackgroundData);
+  }, []);
+
+  return { data, error };
+};
