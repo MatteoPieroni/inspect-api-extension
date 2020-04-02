096dab5a8d289935a753d9e9a51407238d9960a0
Matteo Pieroni <matteopieroni6@gmail.com>
2020-04-03 00:02:21 +0100

    Moved reducer and added refresh of data

--- src/hooks/useTableData.ts
+++ src/hooks/useTableData.ts
@@ -1,151 +1,16 @@
-import { useMemo, useReducer } from "react";
+import { useMemo, useReducer, useEffect } from "react";
 
 import { Data } from "../types";
-import { innerSort } from "../utils/innerSort";
-
-const RESET = "RESET";
-const ORDER = "ORDER";
-const FILTER_DUPLICATES = "FILTER_DUPLICATES";
-const SEARCH = "SEARCH";
-
-interface TableData {
-  headerKeys: (keyof Data)[];
-  data: Data[];
-  orderByKey: (key: keyof Data, isAsc?: boolean) => void;
-  reset: () => void;
-  activeFilter: keyof Data | "";
-  findDuplicates: (key: keyof Data | undefined) => void;
-  search: (keyword: string) => void;
-}
-
-interface TableState {
-  data: Data[];
-  activeFilter: keyof Data | "";
-  activeOrderIsAsc: boolean;
-}
-
-interface ResetAction {
-  type: typeof RESET;
-  payload: { data: Data[] };
-}
-interface OrderAction {
-  type: typeof ORDER;
-  payload: { key: keyof Data; isAsc: boolean };
-}
-interface FilterDuplicatesAction {
-  type: typeof FILTER_DUPLICATES;
-  payload: { key: keyof Data | undefined; data: Data[] };
-}
-interface SearchAction {
-  type: typeof SEARCH;
-  payload: { keyword: string; data: Data[] };
-}
-
-type ReducerAction =
-  | ResetAction
-  | OrderAction
-  | FilterDuplicatesAction
-  | SearchAction;
-
-const initState: (data: Data[]) => TableState = initialData => {
-  return {
-    data: initialData,
-    activeFilter: "",
-    activeOrderIsAsc: true
-  };
-};
-
-const tableDataReducer: React.Reducer<TableState, ReducerAction> = (
-  state,
-  action
-) => {
-  switch (action.type) {
-    case ORDER:
-      return {
-        data: [...state.data].sort((a, b) =>
-          innerSort(a, b, action.payload.key, action.payload.isAsc)
-        ),
-        activeFilter: action.payload.key,
-        activeOrderIsAsc: action.payload.isAsc
-      };
-    case FILTER_DUPLICATES:
-      if (!action.payload.key) {
-        return {
-          ...state,
-          data: action.payload.data
-        };
-      }
-
-      const keyValues: string[] = action.payload.data.map(
-        element => element[action.payload.key as keyof Data]
-      );
-      const duplicatesArray: Data[] = action.payload.data.filter(
-        (element, index) => {
-          if (
-            keyValues.indexOf(
-              element[action.payload.key as keyof Data],
-              index + 1
-            ) > -1
-          ) {
-            return element;
-          }
-
-          // for the last item compare with previous values
-          if (index === action.payload.data.length - 1) {
-            if (
-              keyValues.indexOf(element[action.payload.key as keyof Data]) !==
-              index
-            ) {
-              return element;
-            }
-          }
-        }
-      );
-
-      if (state.activeFilter) {
-        return {
-          ...state,
-          data: [...duplicatesArray].sort((a, b) =>
-            innerSort(a, b, state.activeFilter, state.activeOrderIsAsc)
-          )
-        };
-      }
-
-      return {
-        ...state,
-        data: duplicatesArray
-      };
-    case SEARCH:
-      if (!action.payload.keyword) {
-        return {
-          ...state,
-          data: action.payload.data
-        };
-      }
-
-      const searchResults = state.data.filter(el =>
-        el.url.includes(action.payload.keyword)
-      );
-
-      if (state.activeFilter) {
-        return {
-          ...state,
-          data: [...searchResults].sort((a, b) =>
-            innerSort(a, b, state.activeFilter, state.activeOrderIsAsc)
-          )
-        };
-      }
-
-      return {
-        ...state,
-        data: searchResults
-      };
-    case RESET:
-      return initState(action.payload.data);
-    default:
-      return state;
-  }
-};
+import {
+  initState,
+  tableDataReducer,
+  TableData,
+  ORDER,
+  RESET,
+  SEARCH,
+  FILTER_DUPLICATES,
+  REFRESH_DATA
+} from "./utils/tableReducers";
 
 export const useTableData: (
   originalData: Data[]
@@ -156,6 +21,10 @@ export const useTableData: (
     initState
   );
   const { data, activeFilter } = state;
+
+  useEffect(() => {
+    dispatch({ type: REFRESH_DATA, payload: { data: originalData } });
+  }, [originalData]);
 
   const headerKeys: (keyof Data)[] = useMemo(
     () =>
---
+++ src/hooks/utils/tableReducers.ts
@@ -0,0 +1,190 @@
+import { innerSort } from "../../utils/innerSort";
+import { Data } from "../../types";
+
+export const RESET = "RESET";
+export const ORDER = "ORDER";
+export const FILTER_DUPLICATES = "FILTER_DUPLICATES";
+export const SEARCH = "SEARCH";
+export const REFRESH_DATA = "REFRESH_DATA";
+
+export interface TableData {
+  headerKeys: (keyof Data)[];
+  data: Data[];
+  orderByKey: (key: keyof Data, isAsc?: boolean) => void;
+  reset: () => void;
+  activeFilter: keyof Data | "";
+  findDuplicates: (key: keyof Data | undefined) => void;
+  search: (keyword: string) => void;
+}
+
+interface TableState {
+  data: Data[];
+  activeFilter: keyof Data | "";
+  activeOrderIsAsc: boolean;
+  activeDuplicateFilter: keyof Data | "";
+}
+
+interface ResetAction {
+  type: typeof RESET;
+  payload: { data: Data[] };
+}
+interface OrderAction {
+  type: typeof ORDER;
+  payload: { key: keyof Data; isAsc: boolean };
+}
+interface FilterDuplicatesAction {
+  type: typeof FILTER_DUPLICATES;
+  payload: { key: keyof Data | undefined; data: Data[] };
+}
+interface SearchAction {
+  type: typeof SEARCH;
+  payload: { keyword: string; data: Data[] };
+}
+interface RefreshAction {
+  type: typeof REFRESH_DATA;
+  payload: { data: Data[] };
+}
+
+export type ReducerAction =
+  | ResetAction
+  | OrderAction
+  | FilterDuplicatesAction
+  | SearchAction
+  | RefreshAction;
+
+export const initState: (data: Data[]) => TableState = initialData => {
+  return {
+    data: initialData,
+    activeFilter: "",
+    activeOrderIsAsc: true,
+    activeDuplicateFilter: ""
+  };
+};
+
+const orderData: (
+  array: Data[],
+  filter: keyof Data,
+  isAsc: boolean
+) => Data[] = (array, filter, isAsc) =>
+  array.sort((a, b) => innerSort(a, b, filter, isAsc));
+
+const filterDuplicates: (data: Data[], key: keyof Data) => Data[] = (
+  data,
+  key
+) => {
+  const keyValues: string[] = data.map(element => element[key]);
+
+  return data.filter((element, index) => {
+    if (keyValues.indexOf(element[key], index + 1) > -1) {
+      return element;
+    }
+
+    // for the last item compare with previous values
+    if (index === data.length - 1) {
+      if (keyValues.indexOf(element[key]) !== index) {
+        return element;
+      }
+    }
+  });
+};
+
+export const tableDataReducer: React.Reducer<TableState, ReducerAction> = (
+  state,
+  action
+) => {
+  switch (action.type) {
+    case REFRESH_DATA:
+      let finalData;
+
+      if (state.activeDuplicateFilter) {
+        finalData = filterDuplicates(
+          action.payload.data,
+          state.activeDuplicateFilter
+        );
+      }
+
+      if (state.activeFilter) {
+        finalData = orderData(
+          action.payload.data,
+          state.activeFilter,
+          state.activeOrderIsAsc
+        );
+      }
+
+      return {
+        ...state,
+        data: finalData || action.payload.data
+      };
+    case ORDER:
+      return {
+        ...state,
+        data: orderData(
+          [...state.data],
+          action.payload.key,
+          action.payload.isAsc
+        ),
+        activeFilter: action.payload.key,
+        activeOrderIsAsc: action.payload.isAsc
+      };
+    case FILTER_DUPLICATES:
+      if (!action.payload.key) {
+        return {
+          ...state,
+          activeDuplicateFilter: "",
+          data: action.payload.data
+        };
+      }
+
+      const duplicatesArray = filterDuplicates(
+        action.payload.data,
+        action.payload.key
+      );
+
+      if (state.activeFilter) {
+        return {
+          ...state,
+          activeDuplicateFilter: action.payload.key,
+          data: orderData(
+            [...duplicatesArray],
+            state.activeFilter,
+            state.activeOrderIsAsc
+          )
+        };
+      }
+
+      return {
+        ...state,
+        activeDuplicateFilter: action.payload.key,
+        data: duplicatesArray
+      };
+    case SEARCH:
+      if (!action.payload.keyword) {
+        return {
+          ...state,
+          data: action.payload.data
+        };
+      }
+
+      const searchResults = state.data.filter(el =>
+        el.url.includes(action.payload.keyword)
+      );
+
+      if (state.activeFilter) {
+        return {
+          ...state,
+          data: [...searchResults].sort((a, b) =>
+            innerSort(a, b, state.activeFilter, state.activeOrderIsAsc)
+          )
+        };
+      }
+
+      return {
+        ...state,
+        data: searchResults
+      };
+    case RESET:
+      return initState(action.payload.data);
+    default:
+      return state;
+  }
+};
